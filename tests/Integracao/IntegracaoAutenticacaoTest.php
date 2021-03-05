d4326bad4bf24a6e1ec8bb21b4890446dc7449a1
Silas Stoffel <silasstofel@gmail.com>
2021-03-04 21:49:24 -0300

    Criando testes de integração do endpoint de autenticar.

---
+++ tests/Integracao/IntegracaoAutenticacaoTest.php
@@ -0,0 +1,77 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+// vendor/bin/phpunit --filter 'IntegracaoAutenticacaoTest'
+
+class IntegracaoAutenticacaoTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    private $conta = [
+        'cpfcnpj' => '75639719000176',
+        'titular' => 'Titular 001',
+        'email'   => 'titular@youpay.com.br',
+        'senha'   => '123456',
+        'celular' => '27111122222',
+    ];
+
+    private $formLogin = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->criarContaRequest();
+        $this->formLogin = [
+            'login'    => $this->conta['email'],
+            'password' => $this->conta['senha'],
+        ];
+    }
+
+    public function testDeveLogarComEmailNormalmente()
+    {
+        $this->checkRespostaLoginComSucesso();
+    }
+
+    public function testDeveLogarComCpfCnpjNormalmente()
+    {
+        $this->formLogin['login'] = $this->conta['cpfcnpj'];
+        $this->checkRespostaLoginComSucesso();
+    }
+
+    public function testNaoDeveLogarComSenhaErrada()
+    {
+        $this->formLogin['password'] = 'senha-errada';
+        $response = $this->loginRequest();
+        $response->seeStatusCode(401);
+        $response->seeJson(['error' => true, 'message' => 'Senha inválida.']);
+    }
+
+    public function testNaoDeveLogarSeNaoEncontrarContra()
+    {
+        $this->formLogin['login'] = 'conta-que-nao-existe@youpay.com';
+        $response = $this->loginRequest();
+        $response->seeStatusCode(401);
+        $response->seeJson(['error' => true, 'message' => 'Conta não localizada.']);
+    }
+
+    private function checkRespostaLoginComSucesso()
+    {
+        $response = $this->loginRequest();
+        $response->seeStatusCode(200);
+        $response->seeJson(['titular' => $this->conta['titular']]);
+    }
+
+    private function criarContaRequest()
+    {
+        $response = $this->json('POST', '/v1/contas', $this->conta);
+        return $response;
+    }
+
+    private function loginRequest()
+    {
+        $response = $this->json('POST', '/auth', $this->formLogin);
+        return $response;
+    }
+}
